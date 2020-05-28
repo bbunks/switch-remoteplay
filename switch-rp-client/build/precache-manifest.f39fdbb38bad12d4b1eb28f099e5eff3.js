7fc71c087a531bd58cd68ca0e9ba1d45d706ea9c
Brenden Bunker <brenden.bunker@gmail.com>
2020-05-28 15:27:37 -0600

    added built site to git

---
+++ switch-rp-client/build/precache-manifest.f39fdbb38bad12d4b1eb28f099e5eff3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5fdd4ea2f09eb7a3f1e0f372091c302",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8af03141fd7bede0ed43",
+    "url": "/static/css/main.c5d658ca.chunk.css"
+  },
+  {
+    "revision": "a418128218c97f536605",
+    "url": "/static/js/2.5c5f3fe9.chunk.js"
+  },
+  {
+    "revision": "f65b45f562155f0fa2653b346e3552a6",
+    "url": "/static/js/2.5c5f3fe9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8af03141fd7bede0ed43",
+    "url": "/static/js/main.df608941.chunk.js"
+  },
+  {
+    "revision": "4432ad765fe65f7af47c",
+    "url": "/static/js/runtime-main.2be0aefd.js"
+  }
+]);
